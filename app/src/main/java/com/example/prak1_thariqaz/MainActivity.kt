aa374711b23aa9f0ed146e109452acdc6a1c3e57
Prima06112002 <ramadhan06112002@gmail.com>
2023-10-05 15:06:01 +0700

    First

---
+++ app/src/main/java/com/example/prak1_thariqaz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prak1_thariqaz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
