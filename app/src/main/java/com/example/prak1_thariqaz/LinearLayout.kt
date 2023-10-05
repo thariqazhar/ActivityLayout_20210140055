762912729010871a6bb1fa7a9440d93818b0bf4e
Prima06112002 <ramadhan06112002@gmail.com>
2023-10-05 18:32:36 +0700

    Linear Layout

---
+++ app/src/main/java/com/example/prak1_thariqaz/LinearLayout.kt
@@ -0,0 +1,11 @@
+package com.example.prak1_thariqaz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear)
+    }
+}
